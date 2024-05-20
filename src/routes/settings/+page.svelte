52a2b5b0f0eb32a5c9435133a9816da928ca2c52
Adrin Amin Salehi <adrin.amin@gmail.com>
2024-05-20 12:19:48 +0000

    Add theme picker at settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,26 +1,29 @@
 <script>
 	// import { goto } from '$app/navigation';
+	import { onMount } from "svelte";
+	import { themeChange } from "theme-change";
+
 	const pageName = 'Settings';
+
+	onMount(() => {
+		themeChange(false); // false parameter is required for svelte
+	});
 </script>
 
 <h1 class="font-bold text-4xl">{pageName}</h1>
 <div class="flex flex-col pt-5">
-	<div class="form-control w-52">
-		<label class="cursor-pointer label">
-			<span class="label-text">Remember me</span>
-			<input type="checkbox" class="toggle toggle-primary" checked />
-		</label>
-	</div>
-	<div class="form-control w-52">
-		<label class="cursor-pointer label">
-			<span class="label-text">Remember me</span>
-			<input type="checkbox" class="toggle toggle-secondary" checked />
-		</label>
-	</div>
-	<div class="form-control w-52">
-		<label class="cursor-pointer label">
-			<span class="label-text">Remember me</span>
-			<input type="checkbox" class="toggle toggle-accent" checked />
+	<div>
+		<label class="form-control">
+			<div class="label">
+				<span class="label-text">Choose theme</span>
+			</div>
+			<select class="select select-primary" data-choose-theme>
+				<option value="aqua">default</option>
+				<option value="dark">dark</option>
+				<option value="light">light</option>
+				<option value="synthwave">synthwave</option>
+				<option value="retro">retro</option>
+			</select>
 		</label>
 	</div>
 </div>
