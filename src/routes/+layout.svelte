f67f10c56ab5134b17e2954e2380e4cfc7c8138f
Adrin Amin Salehi <adrinamin@fedora-personal.lan>
2024-06-30 07:50:06 +0200

    Adding firebase support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
 	import '../app.css';
-    import { goto } from '$app/navigation';
-    import { page } from '$app/stores';
-
-    
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 </script>
 
 <div class="container mx-auto px-4 py-4">
-
-    {#if $page.url.pathname === '/'}
-        <div class="text-right">
-            <button class="btn btn-ghost" on:click={() => goto("/settings")}>
-                <i class="fa-solid fa-gear"></i>
-            </button>
-        </div>
-    {:else}
-        <div class="text-left">
-            <button class="btn btn-ghost" on:click={() => goto("/")}>
-                <i class="fa-solid fa-arrow-left"></i>
-            </button>
-        </div>
-    {/if}
+	{#if $page.url.pathname === '/'}
+		<div class="text-right">
+			<button class="btn btn-ghost" on:click={() => goto('/settings')}>
+				<i class="fa-solid fa-gear"></i>
+			</button>
+		</div>
+	{:else}
+		<div class="text-left">
+			<button class="btn btn-ghost" on:click={() => goto('/')}>
+				<i class="fa-solid fa-arrow-left"></i>
+			</button>
+		</div>
+	{/if}
 	<slot />
 </div>
 
